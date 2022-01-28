dd7308d058cf17f8031683f8c2f661c16188ab0a
Strada Fabio <fabio.strada@allitude.it>
2022-01-28 17:23:09 +0100

    init shell project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+        loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:3000/remoteEntry.js',
+            exposedModule: './Module'
+        })
+        .then(m => m.AppModule)  
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+        loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4000/remoteEntry.js',
+            exposedModule: './Module'
+        })
+        .then(m => m.AppModule)  
+  }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
