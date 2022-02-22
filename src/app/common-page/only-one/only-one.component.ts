47eb75986c133858d0165462e655ef0e48778a5f
Strada Fabio <fabio.strada@allitude.it>
2022-02-22 12:51:50 +0100

    fix

--- src/app/common-page/only-one/only-one.component.ts
+++ src/app/common-page/only-one/only-one.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { StateService } from 'my-authenticator-lib';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-only-one',
   templateUrl: './only-one.component.html',
   styleUrls: ['./only-one.component.scss']
 })
-export class OnlyOneComponent implements OnInit {
-
-  public property1!: string;
-
-  constructor(private stateService: StateService) { }
-
-  ngOnInit(): void {
-    this.property1 = this.stateService.getProperty1();
-    console.log(this.stateService.getProperty1());
-  }
+export class OnlyOneComponent {
 
 }
