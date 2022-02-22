575a288ac907d7d188364d62b75704429269246a
Strada Fabio <fabio.strada@allitude.it>
2022-02-22 12:44:34 +0100

    import new version library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component, HostListener } from '@angular/core';
-import { AuthenticatorService, StateService } from 'my-authenticator-lib';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { AuthenticatorService, BaseComponent, EventService } from 'my-authenticator-lib';
+import { takeUntil } from 'rxjs';
 import { RoutingService } from './services/routing.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent extends BaseComponent implements OnInit {
  
-  public property1!: string;
+  public language!: string;
 
   constructor(public authenticator: AuthenticatorService,
-              private routingService: RoutingService) {  
+              private routingService: RoutingService,
+              private eventService: EventService) {  
+      super();
+  }
+
+  ngOnInit(): void {
+      this.eventService.getLanguage()
+          .pipe(takeUntil(this.unsubscribeAll))
+          .subscribe(language => {
+              this.language = language;
+          });
   }
 
   @HostListener('window:beforeunload', ['$event'])
